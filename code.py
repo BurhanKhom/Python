d1b85e5271dbbdbaefdf2d271148ccdd12df973d
Burhanuddin Khomosi <burhanuddinkhomosi@Burhan.local>
2018-02-14 21:58:35 +0530

    first

---
+++ code.py
@@ -0,0 +1,30 @@
+#this code needs optimization
+#never mind, currently in learning stage
+#Author : [ BurhanKhom009 ] as a Beginner in Python
+
+t = int(input());
+loop=1
+while loop <=t :
+	x=0
+	y=0
+	maxCordinates = raw_input().split(' ')
+	dest = raw_input().split(' ')
+	length = int(input())
+	s = raw_input()
+	for i in range(length):
+		if s[i]=='L':
+			x = x - 1
+		elif s[i]=='R':
+			x = x + 1
+		elif s[i]=='U':
+			y = y + 1
+		else:
+			y = y - 1
+	if x==int(dest[0]) and y==int(dest[1]):
+		print "Case %d: REACHED" % loop
+	elif x>=0 and y>=0 and x<int(maxCordinates[0]) and y<int(maxCordinates[1]):
+		print "Case %d: SOMEWHERE" % loop
+	else:
+		print "Case %d: DANGER" % loop
+	loop+=1
+
